5691d99ac385f0e80ead5c12bb3c361940afb295
Michael Schlottke-Lakemper <michael@sloede.com>
2021-01-16 08:24:09 +0100

    Update/amend example elixirs

--- examples/elixir_advection_simple.jl
+++ examples/elixir_advection_simple.jl
@@ -1,39 +1,28 @@
-
+# Load necessary packages
 using OrdinaryDiffEq
 using Trixi
 
-###############################################################################
-# semidiscretization of the linear advection equation
-
+# Create equations
 advectionvelocity = (1.0, 1.0)
 equations = LinearScalarAdvectionEquation2D(advectionvelocity)
 
-# Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
+# Create DGSEM solver for polynomial degree = 3
 solver = DGSEM(3, flux_lax_friedrichs)
 
-coordinates_min = (-1, -1) # minimum coordinates (min(x), min(y))
-coordinates_max = ( 1,  1) # maximum coordinates (max(x), max(y))
-
 # Create a uniformely refined mesh with periodic boundaries
-mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level=4,
-                n_cells_max=30_000) # set maximum capacity of tree data structure
+coordinates_min = (-1, -1) # minimum coordinates
+coordinates_max = ( 1,  1) # maximum coordinates
+mesh_static = TreeMesh(coordinates_min, coordinates_max,
+                       initial_refinement_level=4,
+                       n_cells_max=30_000)
 
-# A semidiscretization collects data structures and functions for the spatial discretization
-semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_convergence_test, solver)
+# Create semidiscretization with all spatial discretization-related components
+semi = SemidiscretizationHyperbolic(mesh, equations,
+                                    initial_condition_convergence_test,
+                                    solver)
 
-
-###############################################################################
-# ODE solvers, callbacks etc.
-
-# Create ODE problem with time span from 0.0 to 1.0
+# Create ODE problem from semidiscretization with time span from 0.0 to 1.0
 ode = semidiscretize(semi, (0.0, 1.0));
 
-
-###############################################################################
-# run the simulation
-
-# OrdinaryDiffEq's `solve` method evolves the solution in time
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
-            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-            save_everystep=false);
+# Evolve ODE problem in time using OrdinaryDiffEq's `solve` method
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false), dt=2.5e-2);
